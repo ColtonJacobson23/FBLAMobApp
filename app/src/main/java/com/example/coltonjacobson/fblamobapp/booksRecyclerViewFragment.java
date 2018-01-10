c1410eed57b72dc42b44048147fa182e27e2f3c9
Unknown <cjjacobson@damonteranchhs.com>
2018-01-09 23:56:50 -0800

    InProgressRecyclerView
    Continuing RecyclerView work tomorrow.

--- app/src/main/java/com/example/coltonjacobson/fblamobapp/booksRecyclerViewFragment.java
+++ app/src/main/java/com/example/coltonjacobson/fblamobapp/booksRecyclerViewFragment.java
@@ -64,6 +64,7 @@ public class booksRecyclerViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_books_recycler_view, container, false);
     }
 
