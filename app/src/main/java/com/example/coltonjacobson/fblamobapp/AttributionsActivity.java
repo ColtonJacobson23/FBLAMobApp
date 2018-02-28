7e5a7b98007e0a6cfd8723c7d82e52995daeec09
Unknown <cjjacobson@damonteranchhs.com>
2018-02-28 03:18:21 -0800

    MinorChanges

---
+++ app/src/main/java/com/example/coltonjacobson/fblamobapp/AttributionsActivity.java
@@ -0,0 +1,13 @@
+package com.example.coltonjacobson.fblamobapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AttributionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attributions);
+    }
+}
