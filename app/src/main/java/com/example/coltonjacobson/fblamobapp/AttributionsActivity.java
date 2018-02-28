8b632305d6c6d642e17d3a32d36b34999097c099
Unknown <cjjacobson@damonteranchhs.com>
2018-02-28 03:18:30 -0800

    MinorChanges

--- app/src/main/java/com/example/coltonjacobson/fblamobapp/AttributionsActivity.java
+++ app/src/main/java/com/example/coltonjacobson/fblamobapp/AttributionsActivity.java
@@ -2,6 +2,8 @@ package com.example.coltonjacobson.fblamobapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AttributionsActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class AttributionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attributions);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    public void onBackClicked() {
+
+        onBackPressed();
     }
 }
